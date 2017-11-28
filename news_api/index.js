a060b0cfc7e4b99a4dbb86fdace7a637630dc1d1
mccurcio <matt.curcio.ri@gmail.com>
2017-11-27 23:41:14 -0500

    updates

--- news_api/index.js
+++ news_api/index.js
@@ -3,41 +3,32 @@
 // NO QUOTES
 // https://newsapi.org/v2/sources?category=business&country=us&language=en&apikey=abc123
 
-/* global $ */
+/*global $*/
 $(document).ready(function() {
   $.ajax({
-      // method: "GET",
+      method: "GET",
       url: "https://newsapi.org/v2/sources",
-      data: {
-        category: "business",
-        country: "us",
-        language: "en",
-        apikey: "46d7b349a95e4d449f27ee7ec153af82"
-      }
+      data: {category: "business",
+             country: "us",
+             language: "en",
+             apikey: "46d7b349a95e4d449f27ee7ec153af82"},
+      success: function(response){
+        if (data.status === "ok"){
+            for (var i = 0; i < response.sources.length; i++){
+                var source = document.createElement("OPTION"); 
+                // createElement"OPTION"
+                source.innerHTML = response.sources[i].name;
+                // news organization
+                document.getElementById("selection").appendChild(source); 
+                // child will be spawned in getElementById("selection")
+                source.setAttribute('value', response.sources[i].id);
+            } 
+        }else{alert(data.message)}
+      }  
+     
     })
-   .done(function(msg) {
-       console.log(msg);
-       console.log(data.status)
+   // .done(function(msg) {
+   //     console.log(msg);
+   //     console.log(data.status)
 })
 
-// NOTES
-// CHECK STATUS OF API
-// function getStatus() {
-//   var url = 'https://newsapi.org/v2/top-headlines?' +
-//     'sources=bbc-news&' +
-//     'apiKey=768e56c66f5b42e88a688a62fa62998c';
-//   var req = new Request(url);
-//   fetch(req)
-//     .then(function(response) {
-//       console.log(response.json());
-//     })
-// }
-// Example returned
-// {"status": "ok",
-// -"articles":
-// ###############
-// $.ajax({
-//   statusCode: {
-//     404: function() {alert( "page not found" )}
-//   }
-// });
